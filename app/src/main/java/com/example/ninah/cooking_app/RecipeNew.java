f571c6bab3c2ad53e80cab99ac8522ad8f8cf35f
RomanEngineer <30973616+RomanEngineer@users.noreply.github.com>
2017-09-18 12:10:15 +0200

    roman

--- app/src/main/java/com/example/ninah/cooking_app/RecipeNew.java
+++ app/src/main/java/com/example/ninah/cooking_app/RecipeNew.java
@@ -46,6 +46,13 @@ public class RecipeNew extends AppCompatActivity implements View.OnClickListener
                 /**
                  * Bitte Funktion einfügen, die dann alles in die DB speichert
                  */
+                int recipeIngredients = Integer.parseInt(recipeIngredientsString);
+                editTextQuantity.setText("");
+                editTextProduct.setText("");
+
+                dbSource.createRecipt(recipeName,  recipeIngredients);
+
+
                 break;
             default:
                 break;
